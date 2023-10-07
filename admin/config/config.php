f6492d623cbdbc5f8555f513db45ebdf97484023
dovietdong <dovietdong2507@gmail.com>
2023-10-08 03:46:42 +0700

    connect heroku

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,6 +1,10 @@
 <?php
-   // $mysqli = new mysqli("ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","z5t36fydvhu88i39","qe3ed1us16l5u4wn","t65c51c1bo351q6m");
-   $mysqli = new mysqli("localhost:3344","root","","dbperfume1");
+    // local
+    //$mysqli = new mysqli("localhost:3344","root","","dbperfume1");
+
+    //heroku
+    $mysqli = new mysqli("en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","dffzsfu69gr3zs1m","wllffodkjt06taig","idf3gr7pfe8qrnts");
+
     // Check connection
     if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
