d41b1321def43ca0099871462b0ac78dbed80f21
dovietdong <dovietdong2507@gmail.com>
2023-10-08 20:14:20 +0700

    backup database

--- config-paypal.php
+++ config-paypal.php
@@ -13,8 +13,8 @@ $currency = "USD";
 define('PAYPAL_SANDBOX', true); //TRUE=Sandbox | FALSE=Production 
 define('PAYPAL_SANDBOX_CLIENT_ID', 'AY4n34WUiajM7q-gc3NWjji_ruqjE7jsq6mBLcPVPe8ILtDYmpUT016DQMddG2PZyS21i3bOge_xiJaC'); 
 define('PAYPAL_SANDBOX_CLIENT_SECRET', 'EPTO424azhkDkovNoOA1FEo1yAkcSr5jqK9VeVqZQnQbrfZyh0fu2Fa4ssmXubynAGEPOnhp7AwYki8E'); 
-define('PAYPAL_PROD_CLIENT_ID', 'AT0f05LN5fpPpztgmcdkeqU3lx5Gw5W3Ogag5vi_ev9-R7WayqNEY817vLik_yLr2D00y58sMYXw1WT3'); 
-define('PAYPAL_PROD_CLIENT_SECRET', 'EGbBcnIJFnlRZ-prfZpPbCF8sR7BFXTHAyuybObBCf7u5AyzHZ0OYvFugC5iDb9lh8b0x_EIOh7da-YI'); 
+define('PAYPAL_PROD_CLIENT_ID', ''); 
+define('PAYPAL_PROD_CLIENT_SECRET', ''); 
   
 // Database configuration  
 // define('DB_HOST', 'kingdomvn.store');  
